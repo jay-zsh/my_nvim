e0fc5c6ac835ce7c9593e3b9fad0908766cf18d1
jay-zsh <p204654297@outlook.com>
2025-06-07 12:22:07 +0800

    changed

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -8,3 +8,17 @@ vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
 -- vim.g.mapleader = " "
 -- vim.g.maplocalleader = ","
 
+--调出终端
+vim.keymap.set('n', '<C-t>', ':botright 8split | terminal<CR>', { noremap = true, silent = true })
+
+
+-- 在终端模式中按 jj 切换到 normal 模式
+vim.keymap.set('t', 'jj', [[<C-\><C-n>]], { noremap = true, silent = true })
+
+-- 在终端模式中按 Esc 切换到 normal 模式
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
+
+-- 模糊查找
+vim.keymap.set('n', '<C-f>', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
+
+
