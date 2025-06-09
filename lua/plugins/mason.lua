b8c7e81567b1089389d3bafc6be0e66b7a242aa1
jay-zsh <p204654297@outlook.com>
2025-06-09 19:00:20 +0800

    back

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,31 +1,30 @@
 return {
-	"mason-org/mason.nvim",
+	"git@github.com:mason-org/mason.nvim.git",
 	event = "VeryLazy", --懒加载后加载
 	dependencies = {
-		"neovim/nvim-lspconfig",
-		"mason-org/mason-lspconfig.nvim",
+		"git@github.com:neovim/nvim-lspconfig.git",
+		"git@github.com:mason-org/mason-lspconfig.nvim.git",
 	},
+	lazy = false,
 	opts = {},
 	config = function(_, opts)
 		require("mason").setup(opts)
 		local registry = require("mason-registry")
 
-local function setup(name, config)
-	local success, package = pcall(registry.get_package, name)
-	if success and not package:is_installed() then
-		package:install()
-	end
+		local function setup(name, config)
+			local success, package = pcall(registry.get_package, name)
+			if success and not package:is_installed() then
+				package:install()
+			end
 
-	local lsp = require("mason-lspconfig").get_mappings().package_to_lspconfig[name]
-	if lsp then
-		config.capabilities = require("cmp").get_lsp_capabilities() -- 修正为正确的模块名
-		config.on_attach = function(client)
-			client.server_capabilities.documentFormattingProvider = false
-			client.server_capabilities.documentRangeFormattingProvider = false
+			local lsp = require("mason-lspconfig").get_mappings().package_to_lspconfig[name]
+			config.capabilities = require("blink.cmp").get_lsp_capabilities() -- 新添加的内容lsp限制配置
+			config.on_attach = function(client)
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentRangeFormattingProvider = false
+			end
+			require("lspconfig")[lsp].setup(config)
 		end
-		require("lspconfig")[lsp].setup(config)
-	end
-end
 
 		local servers = {
 			["lua-language-server"] = {
@@ -38,26 +37,7 @@ end
 				},
 			},
 			pyright = {},
-			["jdtls"] = {
-				cmd = { "jdtls" },
-				root_dir = function(fname)
-					return require("lspconfig.util").root_pattern("pom.xml", "gradle.build", ".git")(fname) 
-						or vim.fn.getcwd()
-				end,
-				settings = {
-					java = {
-						configuration = {
-							runtimes = {
-								{
-									name = "JavaSE-17",
-									path = "/usr/lib/jvm/java-17-openjdk",
-									default = true
-								}
-							}
-						}
-					}
-				}
-			},
+			["jdtls"] = {},
 			["clangd"] = {},
 			["html-lsp"] = {},
 			["css-lsp"] = {},
@@ -76,3 +56,4 @@ end
 		})
 	end,
 }
+
